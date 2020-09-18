a2ffa17def49655a771b3cfc0f4f1416a1245db9
Josh Goldberg <me@joshuakgoldberg.com>
2020-09-18 16:21:55 -0400

    Fix: typestat.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export const withString1 = (arg: string) => arg;
+export const withString1 = (arg) => arg;
 
-export const withString2 = (arg: string) => arg;
+export const withString2 = (arg) => arg;
 
-export const withString3 = (arg: string) => arg;
+export const withString3 = (arg) => arg;
 
 export const usesWithString = () => {
   return [withString1(""), withString2(""), withString3("")];
